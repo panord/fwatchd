333a5b1cffd5ebb106f7581a2dabaeff21529c82
Patrik Lundgren <patrik.lundgren.95@gmail.com>
2022-02-05 11:57:59 +0100

    Add list command which lists the file index
    This currently uses the debug formatter '{:?}' which isn't very
    readable. But it's a start.
    
    Signed-off-by: Patrik Lundgren <patrik.lundgren.95@gmail.com>

--- src/flogctl.rs
+++ src/flogctl.rs
@@ -8,6 +8,20 @@ fn append(args: &ArgMatches, state: &mut State) -> Result<()> {
     do_append(state, &fname)
 }
 
+fn list(args: &ArgMatches, state: &mut State) -> Result<()> {
+    let fname: Option<String> = args.value_of_t("file").ok();
+    if fname.is_some() {
+        let h = state
+            .files
+            .get(&fname.unwrap())
+            .context("Found no such tracked file")?;
+        println!("{:?}", h);
+    } else {
+        println!("{:?}", state.files);
+    }
+    Ok(())
+}
+
 pub fn build() -> clap::App<'static> {
     let mut app = App::new("flog - the forgetful file log.")
         .version("2021")
@@ -15,24 +29,24 @@ pub fn build() -> clap::App<'static> {
         .about("flog has a short but excellent memory, it remembers file(s) by name and \n");
 
     app = app.subcommand(
-        App::new("append")
-            .about("Append file-snapshot to history.")
+        App::new("track")
+            .about("track file, take snapshot when changed.")
             .arg(Arg::new("file").required(true).takes_value(true)),
     );
 
-    //    app = app.subcommand(
-    //        App::new("watch")
-    //            .about("watch file, take snapshot when changed.")
-    //            .arg(Arg::new("file").required(true).takes_value(true)),
-    //    );
+    app = app.subcommand(
+        App::new("list")
+            .about("list available file snapshots")
+            .arg(Arg::new("file").takes_value(true)),
+    );
 
     app
 }
 
 fn dispatch(app: &mut App, matches: &ArgMatches) {
     match matches.subcommand() {
-        Some(("append", sargs)) => append(sargs, &mut load_index()),
-        //        ("watch", sargs) => watch(sargs, &mut load_index()),
+        Some(("track", sargs)) => append(sargs, &mut load_index()),
+        Some(("list", sargs)) => list(sargs, &mut load_index()),
         None => {
             println!("{}", app.render_usage());
             Ok(())
