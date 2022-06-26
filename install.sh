895357fc78e7ac5ece3eb89b2058801edd25d92b
Patrik Lundgren <patrik.lundgren.95@gmail.com>
2022-06-26 13:18:21 +0200

    install: Add installation of binaries to /usr/sbin for systemd service
    Signed-off-by: Patrik Lundgren <patrik.lundgren.95@gmail.com>

--- install.sh
+++ install.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
+
+if test -d /lib/systemd/system/; then
+	cp fwatch.service /lib/systemd/system
+fi
+
 useradd -r -d / -c "File watching daemon" -s /usr/bin/nologin fwatch
+
+install ./target/release/fwatchctl /usr/sbin
+install ./target/release/fwatchd /usr/sbin
